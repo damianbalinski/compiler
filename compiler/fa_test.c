657d3f1da1db1973e540cc36a8654f7a36c4c9c1
damianbalinski <damianbalinski99@gmail.com>
2020-10-09 13:34:16 +0200

    lista 1: zadanie 1
    - automat skonczony, testy do automatu skonczonego

---
+++ compiler/fa_test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "finite_automat.c"
+#define RES(V) printf("%d\n", V)
+
+void test_suffix();
+void test_transition_function();
+void test_pos();
+void test_finite_automat();
+
+int main(void)
+{
+	//test_suffix();				// DONE
+	//test_transition_function();	// DONE
+	//test_pos();					// DONE
+	test_finite_automat();
+
+	return 0;
+}
+
+void test_suffix()
+{
+	// SUFIX TEST
+	printf("SUFIX 1\n");
+	RES(is_suffix("abcdef", 6, "a", 1));
+	RES(is_suffix("abcdef", 6, "b", 1));
+	RES(is_suffix("abcdef", 6, "c", 1));
+	RES(is_suffix("abcdef", 6, "d", 1));
+	RES(is_suffix("abcdef", 6, "e", 1));
+	RES(is_suffix("abcdef", 6, "f", 1));
+	RES(is_suffix("abcdef", 6, "g", 1));
+
+	printf("SUFIX 2\n");
+	RES(is_suffix("abcdef", 6, "ab", 2));
+	RES(is_suffix("abcdef", 6, "bc", 2));
+	RES(is_suffix("abcdef", 6, "cd", 2));
+	RES(is_suffix("abcdef", 6, "de", 2));
+	RES(is_suffix("abcdef", 6, "ef", 2));
+	RES(is_suffix("abcdef", 6, "fg", 2));
+
+	printf("SUFIX 3\n");
+	RES(is_suffix("abcdef", 6, "abc", 3));
+	RES(is_suffix("abcdef", 6, "bcd", 3));
+	RES(is_suffix("abcdef", 6, "cde", 3));
+	RES(is_suffix("abcdef", 6, "def", 3));
+	RES(is_suffix("abcdef", 6, "efg", 3));
+}
+
+void test_transition_function()
+{
+	char* pattern = "abba";
+	int** matrix;
+	int x = 3, y = 5;
+
+	init_transition_function(&matrix, x, y);
+	print_transition_function(matrix, x, y);
+
+	compute_transition_function(matrix, x, y, pattern);
+	print_transition_function(matrix, x, y);
+}
+
+void test_finite_automat()
+{
+	FILE* file = fopen("test2.txt", "rb");
+	char* pattern = "abba";
+
+	if (file == NULL)
+		printf("error\n");
+
+	finite_automat(file, pattern);
+
+	fclose(file);
+}
+
+void test_pos()
+{
+	printf("%d\n", pos('a'));
+	printf("%d\n", pos('b'));
+	printf("%d\n", pos('c'));
+	printf("%d\n", pos('d'));
+}
--- compiler/finite_automat.c
+++ compiler/finite_automat.c
@@ -4,59 +4,86 @@
 #include <string.h>
 #include <stdbool.h>
 
-// TEST
-#define RES(V) printf("%d\n", V)
-
 #define MAX 3
+const char* DICT = "abc";
+
+//TODO delete
+int pos(char c) {
+	return strchr(DICT, c) - DICT;
+}
 
 /**
  * Sprawdzmy, czy str2 jest sufiksem str1, porownujemy
  * ostatnie len2 znakow w obydwu lancuchach.
  */
-bool is_sufix(char* str1, int len1, char* str2, int len2)
+bool is_suffix(char* str1, int len1, char* str2, int len2)
 {
-	// TEST
-	//printf("len1: %d\n", len1);
-	//printf("len2: %d\n", len2);
-	//printf("str1: %s\n", str1);
-	//printf("str2: %s\n", str2);
-
-	//if (strncmp(str2, str1 + len1 - len2, len2) == 0)
-	//	printf("    sufix\n");
-	//else
-	//	printf("    _\n");
-
 	return strncmp(str2, str1 + len1 - len2, len2) == 0;
 }
 
-int** init_transition_function(int x, int y)
+/**
+ * Inicjalizacja funkcji przejsc, dla wygody przekazujemy wskaznik na macierz.
+ */
+void init_transition_function(int*** matrix, int x, int y)
 {
-	int** matrix = (int**)malloc(y * sizeof(int*));
+	*matrix = (int**)malloc(y * sizeof(int*));
 
 	for (int i = 0; i < y; i++)
-		matrix[i] = (int*)malloc(x * sizeof(int));
+		(*matrix)[i] = (int*)malloc(x * sizeof(int));
 
 	return matrix;
 }
 
 /**
- * Obliczamy przyrostowo funkcje przejsc, zaczynamy od q=0
+ * Obliczanie funkcji przejsc, zaczynamy od q=0
  * Szukamy k takiego ze Pk ] Pq?, czyli Pk jest sufiksem Pq?, gdzie
  * Pk - prefiks wzorca P[0, ..., k-1], P0 jest lancuchem pustym
  * Pq - prefiks wzorca P[0, ..., q-1]
- * ?  - pojedynczy znak ze zlownika
- * 
+ * ?  - pojedynczy znak ze slownika
  */
-int** compute_transition_function(int** matrix, int x, int y, char* pattern, int length)
+void compute_transition_function(int** matrix, int x, int y, char* pattern)
 {
-	/**
-	 * Stan poczatkowy - zaczynamy od q=0 oraz wyzerowanego bufora,
-	 * bufor buff bedzie przechowywal lancuch Pq?
-	 * zauwazmy ze maksymalna dlugosc bufora = length + 1
-	 */
 	int q = 0;
-	char* buff = (char)calloc(length + 1, sizeof(char));
+	char* buff;
+	int c;
+	int k;
 
+	/**
+	 * Zaczynamy od q=0 oraz wyzerowanego bufora,
+	 * bufor buff bedzie przechowywal lancuch Pq?, gdzie Pq jest prefikesm
+	 * a ? aktualnie rozpatrywanym znakiem
+	 * zauwazmy ze maksymalna dlugosc bufora = y = length+1, po 
+	 * doliczniu znaku pustego mamy y+1
+	 */
+	buff = (char*)calloc(y+1, sizeof(char));
+
+	/**
+	 * Iterujemy po wszystkich stananch, dla kazdego stanu po wszystkich znakach
+	 */
+	for (q = 0; q < y; q++)
+	{
+		for (c = 0; c < MAX; c++)
+		{
+			// ustawiamy nastepny znak w buforze
+			buff[q] = DICT[c];
+
+			// obliczamy maksymalna sensowna wartosc k
+			k = min(y, q + 1);
+
+			// dekrementujemy k do poki ~ Pk ] Pq?
+			while (!is_suffix(buff, q + 1, pattern, k))
+				k--;
+			
+			// otrzymalismy stan, po jakim przejdziemy po dodaniu znaku ? do prefiksu Pq
+			matrix[q][c] = k;
+		}
+
+		/**
+		 * Po wyjsciu z petli przeszlismy wszystki znaki, przechodzimy do nastepnego
+		 * stanu, ustawiamy prefiks Pq, czyli w miejsce q wstawiamy prawidlowy znak wzorca
+		 */
+		buff[q] = pattern[q];
+	}
 }
 
 void print_transition_function(int** matrix, int x, int y)
@@ -69,78 +96,81 @@ void print_transition_function(int** matrix, int x, int y)
 	}
 }
 
-void transition_function(char* pattern, int length)
-{
-
-
-}
-
 void finite_automat(FILE* file, char* pattern)
 {
-	char byte;
 	int length;
+	int** matrix;
+	int x, y;
+	int c;
+	int q;
+	int counter;
 
+	/**
+	 * Przygotowanie funkcji przejsc.
+	 * x = MAX = dlugosc alfabetu
+	 * y = length+1 = liczba stanow (lacznie ze stanem zerowym)
+	 */
 	length = strlen(pattern);
+	x = MAX;
+	y = length + 1;
+	init_transition_function(&matrix, x, y);
+	compute_transition_function(matrix, x, y, pattern);
 
-	//TODO TEST
-	printf("pattern: %s\n", pattern);
-	printf("length : %d\n", length);
+	/**
+	 * Zaczynamy w stanie q = 0, przeszukujemy caly plik
+	 */
+	q = 0;
+	counter = 0;
 
-	while ((byte = getc(file)) != EOF)
+	// TEST
+	//printf("len: %d\n", length);
+	//printf("x: %d\n", x);
+	//printf("y: %d\n", y);
+	//printf("q: %d\n", q);
+	//printf("couter: %d\n", counter);
+
+
+	while ((c = getc(file)) != EOF)
 	{
-		printf("%c ", byte);
+		//printf("c: %c\n", c);
+		//printf("pos: %d\n", pos(c));
+		//printf("q: %d\n", q);
+		//printf("matrix: %d\n", matrix[q][pos(c)]);
+		//printf("mat2\n");
+
+		printf("%c   ", c);
+		q = matrix[q][pos(c)];
+		if (q == length)
+			printf("PATTERN %d\n", counter);
+		else
+			printf("%d\n", q);
+
+		++counter;
 	}
 }
 
-int main(int argc, char* args[])
-{
-	// data
-	FILE* input = NULL;
-	FILE* output = NULL;
-	char* input_path = NULL;
-	char* output_path = NULL;
-	char* pattern;
+//int main(int argc, char* args[])
+//{
+//	// data
+//	FILE* input = NULL;
+//	FILE* output = NULL;
+//	char* input_path = NULL;
+//	char* output_path = NULL;
+//	char* pattern;
+//
+//	////TODO TEST
+//	//input_path = "test1.txt";
+//	//pattern = "abc";
+//
+//	//// open file
+//	//input = fopen(input_path, "rb");
+//
+//	//// perform pattern searching
+//	//finite_automat(input, pattern);
+//
+//	//// close file
+//	//fclose(input);
+//
+//	return 0;
+//}
 
-	////TODO TEST
-	//input_path = "test1.txt";
-	//pattern = "abc";
-
-	//// open file
-	//input = fopen(input_path, "rb");
-
-	//// perform pattern searching
-	//finite_automat(input, pattern);
-
-	//// close file
-	//fclose(input);
-
-	return 0;
-}
-
-void test()
-{
-	// SUFIX TEST
-	printf("SUFIX 1\n");
-	RES(is_sufix("abcdef", 6, "a", 1));
-	RES(is_sufix("abcdef", 6, "b", 1));
-	RES(is_sufix("abcdef", 6, "c", 1));
-	RES(is_sufix("abcdef", 6, "d", 1));
-	RES(is_sufix("abcdef", 6, "e", 1));
-	RES(is_sufix("abcdef", 6, "f", 1));
-	RES(is_sufix("abcdef", 6, "g", 1));
-
-	printf("SUFIX 2\n");
-	RES(is_sufix("abcdef", 6, "ab", 2));
-	RES(is_sufix("abcdef", 6, "bc", 2));
-	RES(is_sufix("abcdef", 6, "cd", 2));
-	RES(is_sufix("abcdef", 6, "de", 2));
-	RES(is_sufix("abcdef", 6, "ef", 2));
-	RES(is_sufix("abcdef", 6, "fg", 2));
-
-	printf("SUFIX 3\n");
-	RES(is_sufix("abcdef", 6, "abc", 3));
-	RES(is_sufix("abcdef", 6, "bcd", 3));
-	RES(is_sufix("abcdef", 6, "cde", 3));
-	RES(is_sufix("abcdef", 6, "def", 3));
-	RES(is_sufix("abcdef", 6, "efg", 3));
-}
