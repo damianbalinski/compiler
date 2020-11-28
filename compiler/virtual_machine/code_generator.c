d6ac6b5728018ca65846b7253fb48e552544c9e6
damianbalinski <damianbalinski99@gmail.com>
2020-11-28 12:08:02 +0100

    kompilator:
    - maszyna wirtualna
    - generator kodow

---
+++ compiler/virtual_machine/code_generator.c
@@ -0,0 +1,31 @@
+int data_offset = 0;
+int code_offset = 0;
+
+/* Rezerwacja miejsca w segmecie danych,
+   zwraca adres zarezerwowanej pamieci */
+int data_location() {
+    return data_offset++;
+}
+
+
+/* Rezerwacja pamieci w segmencie kodu,
+   zwraca adres zarezerwowanej pamieci */
+int reserve_loc() {
+    return code_offset++;
+}
+
+int gen_label() {
+    return code_offset;
+}
+
+/* Generowanie kodu w bierzacym przesunieciu */
+void gen_code( enum code_ops operation, int arg ) {
+    code[code_offset].op    = operation;
+    code[code_offset++].arg = arg;
+}
+
+/* Generowanie kodu w podanym przesunieciu */
+void back_patch( int addr,  enum code_ops operation, int arg ) {
+    code[addr].op  = operation;
+    code[addr].arg = arg;
+}
