264371aff600c6c40afeb38204b3bd88720a465f
damianbalinski <damianbalinski99@gmail.com>
2021-01-07 15:16:05 +0100

    kompilator 2 - makefile, skrypty testujace

--- compiler/compiler/testall.sh
+++ compiler/compiler/testall.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 path=$1
+dest=$2
 RED='\033[0;31m'
 PURPLE='\033[1;35m'
 NC='\033[0m'
+
+make
+make clean
 
 for file in $(find $path -type f); do 
     echo ""
     echo -e "${PURPLE}###################################${NC}"
     echo -e "${PURPLE}# ${file}${NC}"
     echo -e "${PURPLE}###################################${NC}"
-    ./myprogram compiler $file ./tests/data
+    ./testsingle.sh $file $dest
     echo "press \n for more ..."
     read line
 done
+
+rm ./temp
---
+++ compiler/compiler/testsingle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+src=$1
+dest=$2
+./temp $src $dest
+
+if (( $? == 0 )) ; then
+    # compilation ok
+    maszyna-wirtualna-cln $dest
+    rm $dest
+else
+    # compilation error
+    echo "Compilation error"
+fi
